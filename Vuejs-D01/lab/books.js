2f57155ce1c0c28ba0b2ffc732d310a6afd10403
nader mohammed <nader.mohammed.20x@gmail.com>
2023-04-30 21:17:42 +0300

    labs

---
+++ Vuejs-D01/lab/books.js
@@ -0,0 +1,66 @@
+const books = [
+    {
+        ISBN: "9781593275846",
+        name: "Eloquent JavaScript",
+        category: "Programming",
+        imageUrl: "https://images.pexels.com/photos/185933/pexels-photo-185933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        numPages: 10,
+        author: "Marijn Haverbeke",
+        price: 22.48
+    },
+    {
+        ISBN: "9780061120084",
+        name: "To Kill a Mockingbird",
+        category: "Classic Literature",
+        imageUrl: "https://images.pexels.com/photos/139764/pexels-photo-139764.jpeg",
+        numPages: 336,
+        author: "Harper Lee",
+        price: 9.89
+    },
+    {
+        ISBN: "9780316015844",
+        name: "The Hunger Games",
+        category: "Young Adult Fiction",
+        imageUrl: "https://images.pexels.com/photos/4394514/pexels-photo-4394514.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        numPages: 38,
+        author: "Suzanne Collins",
+        price: 10.89
+    },
+    {
+        ISBN: "9780679720201",
+        name: "1984",
+        category: "Classic Literature",
+        imageUrl: "https://images.pexels.com/photos/5829/man-people-men-vintage-5829.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        numPages: 328,
+        author: "George Orwell",
+        price: 11.99
+    },
+    {
+        ISBN: "9780441172719",
+        name: "Dune",
+        category: "Science Fiction",
+        imageUrl: "https://images.pexels.com/photos/8696402/pexels-photo-8696402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        numPages: 688,
+        author: "Frank Herbert",
+        price: 9.99
+    },
+    {
+        ISBN: "9780804171281",
+        name: "The Girl on the Train",
+        category: "Mystery",
+        imageUrl: "https://images.pexels.com/photos/7014491/pexels-photo-7014491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        numPages: 336,
+        author: "Paula Hawkins",
+        price: 8.49
+    },
+    {
+        ISBN: "9780345391803",
+        name: "Jurassic Park",
+        category: "Science Fiction",
+        imageUrl: "https://images.pexels.com/photos/4337155/pexels-photo-4337155.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        numPages: 448,
+        author: "Michael Crichton",
+        price: 9.99
+    },
+    // Add more books here
+];
